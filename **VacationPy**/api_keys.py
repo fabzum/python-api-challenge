98956932487e7d1d0566e232557695659ff53574
fabzum <60756419+fabzum@users.noreply.github.com>
2020-04-11 18:25:12 -0700

    done

--- **VacationPy**/api_keys.py
+++ **VacationPy**/api_keys.py
@@ -1,5 +1,2 @@
-# OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
-
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB9GfP45fQT7Nu0DVBvLua7ET79NqjSZHM"
---
+++ **WeatherPy**/api_key.py
@@ -0,0 +1,2 @@
+# OpenWeatherMap API Key
+weather_api_key = "4325a6724565d43026ed30efed1a4ba3"
